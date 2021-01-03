21d6610d7742ca844d53cb3646d169980c19fc76
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2021-01-03 17:00:53 -0500

    Show prop in Image.

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const ImageDetail = props => {
     console.log(props);
-    return <Text>Show image of forest</Text>;
+    return <Text>{props.title}</Text>;
 };
 
 const styles = StyleSheet.create({});
