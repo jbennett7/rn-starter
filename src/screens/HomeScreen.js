96e6efad845b6b7f5ded19085832a1782855e5ba
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2021-01-03 16:11:21 -0500

    changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Joe'sPhone</Text>;
+  return (
+      <View>
+          <Text style={styles.text}>Joe's Phone</Text>
+      </View>
+  );
 };
 
 const styles = StyleSheet.create({
