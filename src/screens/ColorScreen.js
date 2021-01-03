b1ec18b9f3e136777b7ae1b072f9ec8e254f9f40
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2021-01-03 18:22:57 -0500

    Random Color in a list.

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, FlatList } from 'react-native';
 
 const ColorScreen = () => {
     const [ colors, setColors ] = useState([]);
@@ -11,13 +11,22 @@ const ColorScreen = () => {
                     setColors([...colors, randomRgb()])
                 }}
             />
-            <View
-                style={{
-                    height: 100,
-                    width: 100,
-                    backgroundColor: randomRgb()
+            <FlatList
+                keyExtractor={(item) => item}
+                data={colors}
+                renderItem={({ item }) => {
+                    return (
+                        <View
+                            style={{
+                                height: 100,
+                                width: 100,
+                                backgroundColor: randomRgb()
+                            }}
+                        />
+                    );
                 }}
             />
+
         </View>
     );
 };
