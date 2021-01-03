7e1d10026c49f8cf6f83b47f9069596ce9a577ec
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2021-01-03 17:41:20 -0500

    Adds CounterScreen boilerplate.

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
+    // TODO: fix this
+    let counter = 0;
     return (
         <View>
+            <Button title="Increase"
+            />
+            <Button title="Decrease"
+            />
             <Text>Current Count:</Text>
         </View>
     );
