8780418258651664f45d889a5da29701bea261a4
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2021-01-03 16:26:48 -0500

    Adds props parameter to HomeScreen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ import {
     TouchableOpacity } from "react-native";
 
 const HomeScreen = (props) => {
-  console.log(props.navagation);
+  console.log(props.navigation);
   return (
       <View>
           <Text style={styles.text}>Joe's Phone</Text>
