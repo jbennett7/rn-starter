cbf5dd8fa57b95974b1206b1a26288e57401ca46
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2021-01-03 17:07:27 -0500

    Adds images and shows a test beach image.

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = props => {
-    console.log(props);
     return (
         <View>
-            <Image source={require('../../assets/beach.jpg')} />
+//            <Image source={require('../../assets/beach.jpg')} />
             <Text>{props.title}</Text>;
         </View>
     );
