5573c4c3a7937877cbeee945fb5e205af4ff6b70
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2021-01-03 21:10:38 -0500

    Square Demo now with reducers.

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -29,28 +29,28 @@ const SquareScreen = () => {
             <ColorCounter
                 onIncrease={() => dispatch({
                     colorToChange: 'red',
-                    amount: COLOR_INCREMENT})
+                    amount: COLOR_INCREMENT})}
                 onDecrease={() => dispatch({
                     colorToChange: 'red',
-                    amount: -1 * COLOR_INCREMENT})
+                    amount: -1 * COLOR_INCREMENT})}
                 color="Red"
             />
             <ColorCounter
                 onIncrease={() => dispatch({
                     colorToChange: 'green',
-                    amount: COLOR_INCREMENT})
+                    amount: COLOR_INCREMENT})}
                 onDecrease={() => dispatch({
                     colorToChange: 'green',
-                    amount: -1 * COLOR_INCREMENT})
+                    amount: -1 * COLOR_INCREMENT})}
                 color="Green"
             />
             <ColorCounter
                 onIncrease={() => dispatch({
                     colorToChange: 'blue',
-                    amount: COLOR_INCREMENT})
+                    amount: COLOR_INCREMENT})}
                 onDecrease={() => dispatch({
                     colorToChange: 'blue',
-                    amount: -1 * COLOR_INCREMENT})
+                    amount: -1 * COLOR_INCREMENT})}
                 color="Blue"
             />
             <View
