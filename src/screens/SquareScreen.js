30ef657a897b545e613ed9e6f5be93b70c61ef63
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2021-01-03 21:18:26 -0500

    Square Demo now with reducers and validators.

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -12,19 +12,19 @@ const reducer = (state, action) => {
     switch(action.colorToChange) {
         case 'red':
             ( state.red + action.amount > 255 ||
-                state.red + action.amount < 0 ) ?
-                    return state :
-                    return { ...state, red: state.red + action.amount };
+                state.red + action.amount < 0 )
+                    ? return state
+                    : return { ...state, red: state.red + action.amount };
         case 'green':
             ( state.green + action.amount > 255 ||
-                state.green + action.amount < 0 ) ?
-                    return state :
-                    return { ...state, red: state.red + action.amount };
+                state.green + action.amount < 0 )
+                    ? return state
+                    : return { ...state, red: state.red + action.amount };
         case 'blue':
             ( state.blue + action.amount > 255 ||
-                state.blue + action.amount < 0 ) ?
-                    return state :
-                    return { ...state, blue: state.blue + action.amount };
+                state.blue + action.amount < 0 )
+                    ? return state :
+                    : return { ...state, blue: state.blue + action.amount };
         default: return state;
     }
 };
